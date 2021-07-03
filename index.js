bf6e0a09ca34e4ba3e70e0c22f7bea625187697f
Gaurang <gaurang@pop-os.localdomain>
2021-07-04 01:00:43 +0530

    Bug Fixes EBS

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 
 
 routes(app);
-
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
 
     console.log("Server IS Running");
 });
